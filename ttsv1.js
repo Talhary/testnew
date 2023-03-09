cd768d160a9d0713b5eae8a2e3d497c47ec065c6
Talhary <talhariaz5425869@gmail.com>
2023-03-09 21:14:28 +0500

    Update ttsv1.js

--- ttsv1.js
+++ ttsv1.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const util = require('util')
-const gtts = require('node-gtts')('en');
-let unlink = util.promisify(fs.unlinkSync)
-module.exports = ttsv1=  async(text,client,pathofsound)=>{
 
+let unlink = util.promisify(fs.unlinkSync)
+module.exports = ttsv1=  async(text,client,pathofsound , lang)=>{
+    const gtts = require('node-gtts')(lang);
 
 
     let filepath =pathofsound;
